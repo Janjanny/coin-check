99c1b3bcb504855237d52685a45fa182378a71e5
Janjanny <janjanbeboso0912@gmail.com>
2024-07-05 01:06:50 +0800

    add icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSun } from '@fortawesome/free-regular-svg-icons'
 
 const Navbar = () => {
+
+    const [isDark, setDark] = useState(true)
+
+
   return (
     <div className='text-white w-10/12 mx-auto py-[2rem] flex justify-between'>
         <a href='/' className=' font-bold text-3xl'>CoinCheck</a>
+        <div className=' cursor-pointer'><FontAwesomeIcon icon={faSun} size='lg' /></div>
 
     </div>
   )
