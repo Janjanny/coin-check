ca6a1bdaf55a7687cccc80202a79badf4f381f70
Janjanny <janjanbeboso0912@gmail.com>
2024-07-04 23:39:52 +0800

    add themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{sans: ['Poppins', 'sans-serif']},
+      colors:{
+        'white': '#ffffff',
+        'black': '#0a0a0a',
+        'gray-1': '#525252',
+        'gray-2': '#333333',
+        'gray-3': '#141414'
+      }
+    },
   },
   plugins: [],
 }
