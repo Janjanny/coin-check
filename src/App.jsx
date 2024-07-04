b40080437659513a3b2626ada884894d7e37622a
Janjanny <janjanbeboso0912@gmail.com>
2024-07-05 00:13:01 +0800

    add component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <>
-      <p className='font-bold text-3xl'>Test</p>
+      <div className="bg-black h-screen">
+        <Navbar/>
+
+      </div>
     </>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='text-white w-10/12 mx-auto py-[2rem] flex justify-between'>
+        <a href='/' className=' font-bold text-3xl'>CoinCheck</a>
+
+    </div>
+  )
+}
+
+export default Navbar
