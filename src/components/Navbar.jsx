141d63f353de78a695ec0d3dd0638f4faf1f7c5a
Janjanny <janjanbeboso0912@gmail.com>
2024-07-05 23:44:35 +0800

    add hero section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSun, faMoon } from '@fortawesome/free-regular-svg-icons'
+import 'boxicons'
 
 const Navbar = () => {
 
@@ -12,10 +13,13 @@ const Navbar = () => {
 
 
   return (
-    <div className='text-white w-10/12 mx-auto py-[2rem] flex justify-between'>
-        <a href='/' className=' font-bold text-3xl'>CoinCheck</a>
-        <div className=' cursor-pointer' onClick={() => {handleSetDark()}}>
-            {!isDark ? <FontAwesomeIcon icon={faSun} size='lg' /> : <FontAwesomeIcon icon={faMoon} size="lg" />}
+    <div className='text-white w-10/12 mx-auto py-[2rem] justify-end flex sticky z-[2] top-0'>
+        {/* <a href='/' className=' font-bold text-3xl'>CoinCheck</a> */}
+        <div className="icons flex items-center gap-5 ">
+          <a href="/" className='flex items-center'><box-icon type='logo' name='github' color="#ffffff" size='sm'></box-icon></a>
+          <div className=' cursor-pointer flex items-center' onClick={() => {handleSetDark()}}>
+              {!isDark ? <box-icon name='sun' color="#ffffff" size='sm' ></box-icon> : <box-icon name='moon' color="#ffffff" size='sm' ></box-icon>}
+          </div>
         </div>
 
     </div>
--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,16 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import 'boxicons';
+
+
 
 const Searchbar = () => {
+
+  const [currency, setCurrency] = useState('USD')
+
   return (
-    <div className='text-white bg-gray-2 p-5 w-fit absolute top-[50%] left-[50%]' style={{transform: 'translate(-50%, -50%)'}}>
-      <div className="input-div">
-        <input type="text" placeholder='Search...' className=' bg-gray-3 text-base py-2 px-3' />
-        <div>
-          USD 
+    <div className="relative w-full h-full bg-black text-white">
+      <div className="hero text-center w-3/4 mx-auto mt-[5rem]">
+        <h1 className='tex'>CoinCheck</h1>
+        <p>Welcome to CoinCheck, your ultimate destination for everything cryptocurrency. Explore the latest trends, insightful analysis, and expert advice on navigating the exciting world of digital currencies.</p>
+      </div>
+      <div className='text-white py-4 px-5 w-fit absolute top-[40%] left-[50%] rounded-xl border-gray-1 border z-[4]' style={{transform: 'translate(-50%, -50%)', background: 'linear-gradient(121deg, rgba(51,51,51,1) 0%, rgba(65,65,65,1) 100%)'}}>
+      <div className="input-div flex items-center gap-4 h-[2.5rem]">
+        <div className=' bg-gray-3 h-full w-[20rem] rounded-lg text-white justify-between flex items-center py-2 px-3'>
+          <input type="text" placeholder='Search...' className=' bg-transparent text-base focus:outline-none' />
+          <div className="icon cursor-pointer flex items-center justify-center"><box-icon name='search' color="#ffffff" ></box-icon></div>
+        </div>
+
+        <div className=' bg-gray-3 h-full flex flex-row items-center gap-1 justify-center rounded-md px-2 cursor-pointer'>
+          {currency}
+          <box-icon name='chevron-down' color="#ffffff" size="sm"></box-icon> 
         </div>  
       </div>
 
+    </div>
     </div>
   )
 }
