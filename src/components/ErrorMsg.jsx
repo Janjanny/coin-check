714ead40d8d870501b128d6d4dda17064b587cbf
Janjanny <janjanbeboso0912@gmail.com>
2024-07-17 22:25:27 +0800

    add dark mode

--- src/components/ErrorMsg.jsx
+++ src/components/ErrorMsg.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import "boxicons";
+import { ThemeContext } from "../ThemeProvider";
 
 const ErrorMsg = ({ isError, setIsError }) => {
+
+  const {theme} = useContext(ThemeContext);
+
+
+
   return (
     <>
       <div className="fixed top-0 left-0 w-screen h-screen z-50">
@@ -9,13 +15,13 @@ const ErrorMsg = ({ isError, setIsError }) => {
         <div className="absolute inset-0 bg-black bg-opacity-50 backdrop-blur-sm"></div>
 
         <div
-          className=" relative z-[50] text-white top-[50%] left-[50%] p-[2rem] rounded-xl w-[20rem]  bg-gradient-to-b from-[#1F1F1F] to-[#2B2B2B] border border-gray-1"
+          className={`relative z-[50] ${theme == 'light' ? 'text-white bg-gradient-to-b from-[#1F1F1F] to-[#2B2B2B] border border-gray-1' : 'text-black bg-[linear-gradient(0deg,_hsla(0,0%,99%,1)_0%,_hsla(0,0%,95%,1)_100%)]'} top-[50%] left-[50%] p-[2rem] rounded-xl w-[20rem] `}
           style={{
             transform: "translate(-50%, -50%)",
           }}
         >
           <div className="icon-div flex justify-center items-center mb-5">
-            <box-icon name="error-circle" color="#ffffff" size="lg"></box-icon>
+            <box-icon name="error-circle" color={theme == 'light' ? '#ffffff' : '#111111'} size="lg"></box-icon>
           </div>
           <p className="text-center">
             Pleaes enter the correct name of the coin.
